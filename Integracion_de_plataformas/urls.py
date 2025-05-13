cddea189a6e0b49ced72a96cdeef69ffd907b20a
Maple-uu <gon.gallardo@duocuc.cl>
2025-05-13 12:58:16 -0400

    Redireccion /api

--- Integracion_de_plataformas/urls.py
+++ Integracion_de_plataformas/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('ferremas.urls')),
+    path('', RedirectView.as_view(url='/api/', permanent=False)),
 ]
 
 if settings.DEBUG:
