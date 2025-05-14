5adaa0770f7b59b163601dc843afc0127a75d978
Maple-uu <gon.gallardo@duocuc.cl>
2025-05-13 21:23:11 -0400

    Fix API Webpay

--- Integracion_de_plataformas/urls.py
+++ Integracion_de_plataformas/urls.py
@@ -3,11 +3,13 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.generic import RedirectView
+from ferremas.views import confirmar_pago
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('ferremas.urls')),
+    path('webpay/confirmar/', confirmar_pago, name='webpay_confirmar'),
     path('', RedirectView.as_view(url='/api/', permanent=False)),
 ]
 
